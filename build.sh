f82b85c605ca84e47f7a1c686e9ae3771216cc23
Kalanza <kalanzavictor@gmail.com>
2025-11-15 19:20:26 +0300

    Update build script to run migrations and populate data

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
-
 # Run migrations
 python manage.py migrate --no-input
+
+# Populate initial data (only if tables are empty)
+python manage.py populate_data || true
+
+# Collect static files
+python manage.py collectstatic --no-input
